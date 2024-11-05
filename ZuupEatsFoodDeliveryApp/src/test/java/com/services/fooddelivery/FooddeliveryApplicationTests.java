ecbfcea6d6dd82516ec61cc4a689fd64df272c1b
Utkarsh Patki <utkarshpatki50@gmail.com>
2024-11-05 23:29:08 +0530

    Added ZuupEatsFoodDeliveryApp.
    Signed-off-by: Utkarsh Patki <utkarshpatki50@gmail.com>

---
+++ ZuupEatsFoodDeliveryApp/src/test/java/com/services/fooddelivery/FooddeliveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.services.fooddelivery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FooddeliveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
